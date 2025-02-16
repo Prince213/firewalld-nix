eaa07cf21030ef02262160366b391c9ae47605ff
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-16 19:11:36 +0800

    feat(package.nix): add src

--- package.nix
+++ package.nix
@@ -1,8 +1,16 @@
 {
+  fetchFromGitHub,
   stdenv,
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "firewalld";
   version = "2.3.0";
+
+  src = fetchFromGitHub {
+    owner = "firewalld";
+    repo = "firewalld";
+    rev = "v${version}";
+    hash = "sha256-ubE1zMIOcdg2+mgXsk6brCZxS1XkvJYwVY3E+UXIIiU=";
+  };
 }
