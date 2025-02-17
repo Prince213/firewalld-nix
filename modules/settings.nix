9ce1c5c3fbbffc8fac5aac5588cda26100c7f5af
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-17 14:10:04 +0800

    feat(modules/settings.nix): add options

--- modules/settings.nix
+++ modules/settings.nix
@@ -18,6 +18,85 @@ in
     type = lib.types.submodule {
       freeformType = format.type;
       options = {
+        DefaultZone = lib.mkOption {
+          type = lib.types.nonEmptyStr;
+          default = "public";
+        };
+        CleanupOnExit = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+        };
+        CleanupModulesOnExit = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+        };
+        IPv6_rpfilter = lib.mkOption {
+          type = lib.types.enum [
+            "strict"
+            "loose"
+            "strict-forward"
+            "loose-forward"
+            "no"
+          ];
+          default = "strict";
+        };
+        IndividualCalls = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+        };
+        LogDenied = lib.mkOption {
+          type = lib.types.enum [
+            "all"
+            "unicast"
+            "broadcast"
+            "multicast"
+            "off"
+          ];
+          default = "off";
+        };
+        FirewallBackend = lib.mkOption {
+          type = lib.types.enum [
+            "nftables"
+            "iptables"
+          ];
+          default = "nftables";
+        };
+        FlushAllOnReload = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+        };
+        ReloadPolicy = lib.mkOption {
+          type =
+            let
+              policy = lib.types.enum [
+                "DROP"
+                "REJECT"
+                "ACCEPT"
+              ];
+            in
+            lib.types.either policy lib.types.commas;
+          default = "INPUT:DROP,FORWARD:DROP,OUTPUT:DROP";
+        };
+        RFC3964_IPv4 = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+        };
+        StrictForwardPorts = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+        };
+        NftablesFlowtable = lib.mkOption {
+          type = lib.types.separatedString " ";
+          default = "off";
+        };
+        NftablesCounters = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+        };
+        NftablesTableOwner = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+        };
       };
     };
   };
